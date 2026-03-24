e357e0875db51ef2693715e797ca086679f38ba9
Cris <crisheight@gmail.com>
2026-03-24 14:25:17 -0500

    Update Sentry configuration: add Unsplash to image domains and enable additional source map options

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+import { withSentryConfig } from "@sentry/nextjs";
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
@@ -19,44 +21,32 @@ const coreConfig = {
         hostname: "utfs.io",
         pathname: "/f/**",
       },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
     ],
   },
 };
 
-import {withSentryConfig} from "@sentry/nextjs";
+const config = withSentryConfig(coreConfig, {
+  org: "criscoded",
+  project: "cloud-media-engine",
 
-const config = withSentryConfig(
-    coreConfig, {
-      // For all available options, see:
-      // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
 
-      org: "criscoded",
-      project: "cloud-media-engine",
+  // Upload a larger set of source maps for prettier stack traces
+  widenClientFileUpload: true,
 
-      // Only print logs for uploading source maps in CI
-      silent: !process.env.CI,
+  // Route browser requests to Sentry through a Next.js rewrite
+  tunnelRoute: "/monitoring",
 
-      // For all available options, see:
-      // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+  // Automatically tree-shake Sentry logger statements
+  disableLogger: true,
 
-      // Upload a larger set of source maps for prettier stack traces (increases build time)
-      widenClientFileUpload: true,
-
-      // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-      // This can increase your server load as well as your hosting bill.
-      // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-      // side errors will fail.
-      tunnelRoute: "/monitoring",
-
-      // Automatically tree-shake Sentry logger statements to reduce bundle size
-      disableLogger: true,
-
-      // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-      // See the following for more information:
-      // https://docs.sentry.io/product/crons/
-      // https://vercel.com/docs/cron-jobs
-      automaticVercelMonitors: true,
-    },
-);
+  // Enable automatic instrumentation of Vercel Cron Monitors
+  automaticVercelMonitors: true,
+});
 
 export default config;
